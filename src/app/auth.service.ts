6c3047a84dede293fe142400d7c9ccf8d4bf8bc9
John Doe <hind.aitouna@gmail.com>
2022-11-16 16:04:19 +0100

    A1

---
+++ src/app/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { delay, Observable, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  isLoggedIn: boolean = false;
+  redirectUrl: string;
+
+  login(name: string, password: string): Observable<boolean> {
+    let isLoggedIn = (name === 'Hind' && password === '123');
+
+    return of(isLoggedIn).pipe(
+      delay(1000),
+      tap(isLoggedIn => this.isLoggedIn = isLoggedIn)
+    );
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+
+}
